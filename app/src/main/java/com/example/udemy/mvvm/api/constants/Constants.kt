9adf2a38545b7df0aedd066653da59bb0d9e6cd6
yunsung <12345ggh81@gmail.com>
2021-09-01 20:51:30 +0900

    [CREATE] API Data Class

---
+++ app/src/main/java/com/example/udemy/mvvm/api/constants/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.udemy.mvvm.api.constants
+
+class Constants {
+
+    companion object{
+
+        const val API_KEY = "f926b9f4bd674b5b819f9ea455a7b45b"
+        const val BASE_URL = "https://api.spoonacular.com"
+    }
+}
