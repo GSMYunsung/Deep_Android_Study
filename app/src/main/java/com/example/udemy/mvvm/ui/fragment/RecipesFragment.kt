86b1574a5f7eec0e28b77647a417a6bc6b4ca607
yunsung <12345ggh81@gmail.com>
2021-09-01 20:51:39 +0900

    [UPDATE] UI Location update

--- app/src/main/java/com/example/udemy/mvvm/ui/fragment/RecipesFragment.kt
+++ app/src/main/java/com/example/udemy/mvvm/ui/fragment/RecipesFragment.kt
@@ -1,20 +1,26 @@
-package com.example.udemy.mvvm.fragment
+package com.example.udemy.mvvm.ui.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.udemy.R
+import com.example.udemy.databinding.FragmentRecipesBinding
 
 class RecipesFragment : Fragment() {
-
+    private lateinit var binding : FragmentRecipesBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_recipes, container, false)
+
+        binding.recyclerview.showShimmer()
+
+        return binding.root
     }
 
 }
