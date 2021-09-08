e9bf61a91c05a120a625dcbea46a4d00f3d135ba
yunsung <12345ggh81@gmail.com>
2021-09-08 21:12:47 +0900

    [CREATE] Network check Logic

---
+++ app/src/main/java/com/example/udemy/mvvm/MainViewModel.kt
@@ -0,0 +1,72 @@
+package com.example.udemy.mvvm
+
+import android.app.Application
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.udemy.mvvm.models.FoodRecipe
+import com.example.udemy.mvvm.util.NetWorkResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.handleCoroutineException
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import java.lang.Exception
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val repository: Repository,
+    application: Application
+    ) : AndroidViewModel(application) {
+
+    //네트워크값이 항상 변할 수 있음
+    var recipesResponse : MutableLiveData<NetWorkResult<FoodRecipe>> = MutableLiveData()
+
+    fun getRecipes(queries: Map<String,String>) = viewModelScope.launch{
+        getRecipesSafeCall(queries)
+    }
+
+    private suspend fun getRecipesSafeCall(queries: Map<String, String>) {
+        if (hasInternetConnection()){
+            try {
+                //만약 여기서 와이파이가 연결 즉, (true) 값이라면 데이터를 api에 값을 보내주는 RemoteDataSource에 보내준다.
+                val response = repository.remote.getRecipes(queries)
+                recipesResponse.value = handleFoodRecipesResponse(response)
+            }catch (e:Exception){
+
+            }
+        }
+        else{
+            //만약 여기서 와이파이가 연결안됨 즉, (false) 값이라면 에러 메시지를 NetWorkResult class에 보내준다.
+            recipesResponse.value = NetWorkResult.Error("No Internet Connection 연결오류")
+        }
+    }
+
+    //api로 부터 응답을 받음
+    private fun handleFoodRecipesResponse(response: Response<FoodRecipe>): NetWorkResult<FoodRecipe>? {
+
+    }
+
+    private fun hasInternetConnection() : Boolean{
+            //디바이스의 네트워크 기능 받기
+            val connectivityManager = getApplication<Application>().getSystemService(
+                Context.CONNECTIVITY_SERVICE
+            ) as ConnectivityManager
+
+            //네트워크 연결
+            val activityNetwork = connectivityManager.activeNetwork ?: return false
+            //네트워크 기능
+            val capabilities = connectivityManager.getNetworkCapabilities(activityNetwork) ?: return false
+            return when{
+                //네트워크에 연결 되어있으면 true 연결되어있지 않으면 false
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        }
+}
---
+++ app/src/main/java/com/example/udemy/mvvm/util/NetWorkResult.kt
@@ -0,0 +1,14 @@
+package com.example.udemy.mvvm.util
+
+import java.security.MessageDigest
+
+sealed class NetWorkResult<T>(
+    val data : T? = null,
+    val message : String? = null
+) {
+
+    class Success<T>(data: T) : NetWorkResult<T>(data)
+    class Error<T>(message: String?,data: T? = null) : NetWorkResult<T>(data,message)
+    class Loading<T> : NetWorkResult<T>()
+
+}
